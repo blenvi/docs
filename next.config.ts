245c85a668c6cc1580274372e772ab2a37eb4b0f
Sohom <sohommondalofficial@gmail.com>
2025-10-21 15:24:27 +0530

    feat: add MDX support to Next.js configuration

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import { createMDX } from 'fumadocs-mdx/next';
+import type { NextConfig } from 'next';
+
+const withMDX = createMDX();
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+};
+
+export default withMDX(nextConfig);
